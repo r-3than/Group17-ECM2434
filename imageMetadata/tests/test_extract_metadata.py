393440de59268dc0d920ffff01ea6a9596fedece
tom-newbold <thomas_newbold@outlook.com>
2023-02-23 20:25:59 +0000

    removed directory logging

--- imageMetadata/tests/test_extract_metadata.py
+++ imageMetadata/tests/test_extract_metadata.py
@@ -1,7 +1,6 @@
 import pytest
 from PIL import Image
 from datetime import datetime as dt
-import os
 
 from extract_metadata import extract_metadata, extract_metadata_from_filepath
 
@@ -9,7 +8,6 @@ TEST_IMG_FILEPATH = 'IMG_1379.JPG'
 CARDINAL = ['N', 'E', 'S', 'W']
 
 def test_extract_metadata():
-    print(os.listdir(os.getcwd()))
     source_img = Image.open(TEST_IMG_FILEPATH)
     output = extract_metadata(source_img)
     assert type(output['DateTime']) == dt
