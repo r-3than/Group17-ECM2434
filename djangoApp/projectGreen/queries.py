f17c121b10ab3b06c1d22089d6c491be8f83c599
tom-newbold <thomas_newbold@outlook.com>
2023-02-26 14:30:38 +0000

    added docstrings and changed function names to match project standards

--- djangoApp/projectGreen/queries.py
+++ djangoApp/projectGreen/queries.py
@@ -4,6 +4,9 @@ DATABASE_PATH = "db.sqlite3"
 
 class Queries:
 	def fetch_query(sql):
+		'''
+		For SELECT queries
+		'''
 		connection = sqlite3.connect(DATABASE_PATH)
 		cursor = connection.cursor()
 		text = (sql)
@@ -13,6 +16,9 @@ class Queries:
 		return result
 	
 	def modify_query(sql):
+		'''
+		For INSERT INTO and DELETE FROM queries
+		'''
 		connection = sqlite3.connect(DATABASE_PATH)
 		cursor = connection.cursor()
 		text = (sql)
@@ -21,33 +27,27 @@ class Queries:
 		connection.close()
 
 
-def addUser(email, name, superuser):
+def add_user(email, name, superuser):
 	username = email.split('@')
 	query = "INSERT INTO Users VALUES ('"+ email + "','" + username[0] + "','" + name + "','" + superuser + "');"
 	Queries.modify_query(query)
 
-def getUsername(email):
+def get_username(email):
 	query = "SELECT username FROM Users WHERE email = '" + email + "';"
 	result = Queries.fetch_query(query)
 	return result[0]
 
-def getName(email):
+def get_name(email):
 	query = "SELECT name FROM Users WHERE email = '" + email + "';"
 	result = Queries.fetch_query(query)
 	return result[0]
 
-def isSuperuser(email):
+def is_superuser(email):
 	query = "SELECT superuser FROM Users WHERE email = '" + email + "';"
 	result = Queries.fetch_query(query)
-	'''
-	isSuperuser = False
-	if result[0] == 1:
-		isSuperuser = True 
-	return isSuperuser
-	'''
-	return True if result[0]==1 else False # inline
+	return True if result[0]==1 else False
 
-def getAllEmails():
+def get_all_emails():
 	query = "SELECT email FROM Users;"
 	result = Queries.fetch_query(query)
 
@@ -56,47 +56,47 @@ def getAllEmails():
 		emails.append(result[i][0])
 	return emails
 
-def deleteUser(email):
+def delete_user(email):
 	query = "DELETE FROM Users WHERE email='" + email + "';"
 	Queries.modify_query(query)
 
-def addFriend(email, friendEmail):
+def add_friend(email, friendEmail):
 	username = email.split('@')
 	query = "INSERT INTO Friends VALUES ('"+ email + "','" + friendEmail + "');"
 	Queries.modify_query(query)
 
-def deleteFriend(email, FriendEmail):
+def delete_friend(email, FriendEmail):
 	query = "DELETE FROM Friends WHERE email='" + email + "' AND friend_email='" + FriendEmail + "';"
 	Queries.modify_query(query)
 
-def addChallenge(challengeId, description):
+def add_challenge(challengeId, description):
 	query = "INSERT INTO Challenges VALUES ('"+ challengeId + "','" + description + "');"
 	Queries.modify_query(query)
 
-def deleteChallenge(challengeId):
+def delete_challenge(challengeId):
 	query = "DELETE FROM Challenges WHERE challengeId='" + challengeId + "';"
 	Queries.modify_query(query)
 
-def addActiveChallenge(challengeDate, challengeId, expired):
+def add_active_challenge(challengeDate, challengeId, expired):
 	query = "INSERT INTO ActiveChallenges VALUES ('"+ challengeDate + "','" + challengeId + "','" + expired + "');"
 	Queries.modify_query(query)
 
-def deleteActiveChallenge(challengeDate):
+def delete_active_challenge(challengeDate):
 	query = "DELETE FROM ActiveChallenges WHERE challengeDate='" + challengeDate + "';"
 	Queries.modify_query(query)
 
-def addSubmission(photoPath, email, challengeDate):
+def add_submission(photoPath, email, challengeDate):
 	query = "INSERT INTO Submission VALUES ('"+ photoPath + "','" + email + "','" + challengeDate + "');"
 	Queries.modify_query(query)
 
-def deleteSubmission(photoPath):
+def delete_submission(photoPath):
 	query = "DELETE FROM Submission WHERE photoPath='" + photoPath + "';"
 	Queries.modify_query(query)
 
-def addVote(email, photoPath):
+def add_vote(email, photoPath):
 	query = "INSERT INTO Upvote VALUES ('"+ email + "','" + photoPath + "');"
 	Queries.modify_query(query)
 
-def removeVote(email, photoPath):
+def remove_vote(email, photoPath):
 	query = "DELETE FROM Upvote WHERE email='" + email + "' AND photoPath='" + photoPath + "';"
 	Queries.modify_query(query)
