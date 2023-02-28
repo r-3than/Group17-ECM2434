b19ad7b83f0aeb61a18fad2b5b9694f0006377c0
tom-newbold <thomas_newbold@outlook.com>
2023-02-28 15:48:53 +0000

    modified for new database structure yet to be implemented

--- djangoApp/projectGreen/points.py
+++ djangoApp/projectGreen/points.py
@@ -1,12 +1,9 @@
-from queries import Queries, add_vote
+from queries import Queries
+from datetime import datetime as dt
 import math
 
 from projectGreen.settings import MICROSOFT
-#DOMAIN = 'exeter.ac.uk'
-DOMAIN = MICROSOFT['valid_email_domains'][0]
-
-# NOTE photo storage `photos\{challenge-date}\{username}.png`
-# would allow database restructure to identify submission by challenge date/user
+DOMAIN = MICROSOFT['valid_email_domains'][0] # = 'exeter.ac.uk'
 
 SCORES = {'submission':10, 'upvote':{'given':1, 'recieved':2}}
 
@@ -14,24 +11,11 @@ def username_to_email(username: str) -> str:
     return username + '@' + DOMAIN
 
 def email_to_username(email: str) -> str:
-    return email.split('@')[0]
+    #return email.split('@')[0]
+    return email.strip('@'+DOMAIN)
 
 ''' OLD
 def recalculate_user_points(username: str) -> int:
-    email = username_to_email(username)
-    points = 0
-    SCORES = {'submission':10, 'upvote':1}
-    for table in SCORES:
-        result = Queries.fetch_query("SELECT * FROM {table} WHERE email='{email}';".format(table.capitalize(), email))
-        points += len(result) * SCORES[table.lower()]
-    return points
-'''
-
-def recalculate_user_points(username: str) -> int:
-    '''
-    Calculated the total points for the user, based on submissions and interactions
-    To be used on database initialization, or in the case of a desynchronous database
-    '''
     email = username_to_email(username)
     points = 0
 
@@ -48,9 +32,29 @@ def recalculate_user_points(username: str) -> int:
         points += len(result) * SCORES['upvote']['recieved']
     
     return points
-# TODO this will be stored in database and fetched to prevent continual recalculation
-# TODO incorporate "lateness" of submission
+'''
 
+def recalculate_user_points(username: str) -> int: # new database structure; TODO get functions for these queries
+    '''
+    Calculated the total points for the user, based on submissions and interactions
+    To be used on database initialization, or in the case of a desynchronous database
+    '''
+    points = 0
+    result = Queries.fetch_query("SELECT * FROM Upvote WHERE username='{username}';".format(username))
+    # fetches all upvotes given by a user
+    points += len(result) * SCORES['upvote']['given']
+    result = Queries.fetch_query("SELECT username, challenge_date, minuites_late FROM Submission WHERE username='{username}';".format(username))
+    # fetches photoPath (identifier) for all user's submissions
+    submissions = [result[i][0:1] for i in range(len(result))] # check this line
+    for username, challenge_date, minuites_late in submissions:
+        time_for_challenge = Queries.fetch_query("SELECT time_frame_minuites FROM ActiveChallenges WHERE challenge_date='{challenge_date}';".format(challenge_date))[0]
+        points += SCORES['submission'] * math.sqrt(math.max(time_for_challenge-minuites_late, 0)+1) # punctuality scaling for submission points
+        result = Queries.fetch_query("SELECT * FROM Upvote WHERE username='{username}' AND challenge_date='{challenge_date}';".format(username, challenge_date))
+        # fetches all upvotes on each submission
+        points += len(result) * SCORES['upvote']['recieved']
+    return points
+
+""" MORE OLD CODE
 def upvote_callback(email: str, photo_path: str):
     '''
     To be triggered by an upvote event:
@@ -66,11 +70,39 @@ def upvote_callback(email: str, photo_path: str):
     # update interaction sum for submission
     target_user = Queries.fetch_query("SELECT email FROM Submission WHERE photoPath='{photo_path}';".format(photo_path))
     target_user = target_user.split('@')
+    ''' FOR POPULARITY METRIC
     target_user_friends = Queries.fetch_query("SELECT * FROM Friends WHERE email='{target_user}' OR friendEmail='{target_user}';".format(target_user))
     target_user_points = recalculate_user_points(target_user.split('@')[0])
     lamdba_user = math.log( 1 + len(target_user_friends) ) * math.sqrt(target_user_points / SCORES['submission'])
-    # TODO add this to database
+    '''
 
 def upvote_user_submission(self_username: str, target_username: str):
     photo_path = Queries.fetch_query("SELECT photoPath FROM Submission WHERE email='{email}';".format(email=username_to_email(target_username)))
     upvote_callback(username_to_email(self_username), photo_path)
+"""
+
+def upvote_callback(submission_username: str, submission_date: dt, voter_username: str):
+    '''
+    To be triggered by an upvote event:
+        - Adds vote to the database
+        - Updates the points of the user making the upvote
+        - Updates popularity metric for the submission recieving the upvote
+    '''
+    # update table
+    Queries.modify_query("INSERT INTO Upvote VALUES ('{submission_username}', '{submission_date}', '{voter_username}');".format(submission_username, submission_date.strtime('%Y-%m-%d'), voter_username))
+    # add to users points
+    user_points = Queries.fetch_query("SELECT points FROM Users WHERE username='{username}';".format(username=submission_username))[0] # correct index ??
+    user_points += SCORES['upvote']['recieved']
+    Queries.modify_query("UPDATE Users SET points={points} WHERE username='{username}';".format(points=user_points, username=submission_username))
+    user_points = Queries.fetch_query("SELECT points FROM Users WHERE username='{username}';".format(username=voter_username))[0]
+    user_points += SCORES['upvote']['given']
+    Queries.modify_query("UPDATE Users SET points={points} WHERE username='{username}';".format(points=user_points, username=voter_username))
+    # TODO update interaction sum for submission here
+
+if __name__=='__main__':
+    result = Queries.fetch_query("SELECT username FROM Users;")
+    users = [result[i][0] for i in range(len(result))]
+    for username in users:
+        points = recalculate_user_points(username)
+        Queries.modify_query("UPDATE Users SET points={points} WHERE username='{username}';".format(points, username))
+        #Queries.fetch_query("SELECT points FROM Users WHERE username='{username}';".format(username))
