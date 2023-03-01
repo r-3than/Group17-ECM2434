df375a5f7e11cad790ebfb27fdb6c174ed869ee7
olijarrett <107365115+olijarrett@users.noreply.github.com>
2023-03-01 22:17:42 +0000

    UI for Prototype Presentation
    * Initial Boostrap template linking
    
    * Install requirements command note
    
    * Login Page
    
    * WIP 27/2 for Steven
    
    * Desktop homepage finished mock up
    
    * Sizing tweaks & Hardcoded data for presentation

--- djangoApp/projectGreen/projectGreen/urls.py
+++ djangoApp/projectGreen/projectGreen/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from projectGreen.views import home,camera, specific_group_access
+from projectGreen.views import home,challenge,camera, specific_group_access
 
 urlpatterns = [
-    path('', home, ),
+    path('', challenge, ),
+    path('home/', home, name='home'),
     path('camera/',camera),
     path('specific_group_access', specific_group_access, ),
     path('admin/', admin.site.urls),
