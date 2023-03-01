7f61e699ba3622f5435482f77fa388f35d7ed8b0
tom-newbold <thomas_newbold@outlook.com>
2023-03-01 18:45:04 +0000

    email authentication settings

--- djangoApp/projectGreen/projectGreen/settings.py
+++ djangoApp/projectGreen/projectGreen/settings.py
@@ -72,6 +72,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'projectGreen.wsgi.application'
 
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'djangotestemail31@gmail.com'  
+EMAIL_HOST_PASSWORD = 'nrsrhztfmmwyqzey'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
