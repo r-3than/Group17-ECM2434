156e42b61f87c2ca51580822bdf216a3d6212bc5
tom-newbold <thomas_newbold@outlook.com>
2023-03-07 15:30:11 +0000

    added pending flag for friends also submission is removed if deemed unsuitable

--- djangoApp/projectGreen/projectGreen/admin.py
+++ djangoApp/projectGreen/projectGreen/admin.py
@@ -62,7 +62,7 @@ def approve_submission(modeladmin, request, queryset):
 @admin.action(description='Remove Submission(s)')
 def deny_submission(modeladmin, request, queryset):
     for submission in queryset:
-        submission.delete()
+        submission.review_submission(False)
 
 
 @receiver(post_save, sender=Upvote)
@@ -79,7 +79,7 @@ def upvote_callback_handler(sender, instance, **kwargs):
 
 @receiver(post_delete, sender=Submission)
 def submission_callback_handler(sender, instance, **kwargs):
-    remove_submission(instance, False)
+    if not instance.reviewed: remove_submission(instance, False)
 
 
 class ProfileInline(admin.StackedInline):
