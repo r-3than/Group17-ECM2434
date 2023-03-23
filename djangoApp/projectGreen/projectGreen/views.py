3fa235de39dcc2d1483afa34d40a4700ba429d06
tom-newbold <thomas_newbold@outlook.com>
2023-03-23 08:52:14 +0000

    cant buy multiple items

--- djangoApp/projectGreen/projectGreen/views.py
+++ djangoApp/projectGreen/projectGreen/views.py
@@ -872,7 +872,7 @@ def buy_item(request):
             item_name = data['item_name']
             spendable_points = data['spendable_points']
             item = StoreItem.objects.get(item_name=item_name)
-            if int(spendable_points) >= item.cost:
+            if int(spendable_points) >= item.cost and (not OwnedItem.owns_item(item_name, username)):
                 item_instance = OwnedItem(item_name=item_name, username=username, is_active=False)
                 item_instance.save()
                 p = Profile.get_profile(username)
